82593d217432ee32b8059580ccb8d16c5802661e
Mike <mcewen1984@hotmail.com>
2022-02-06 07:09:37 -0800

    adding hero with image and CTA

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Page from "./components/Page";
+import Home from "./views/Home";
 
 import Routes from "./Routes";
 
 function App() {
     return (
         <Page>
-            <div>Hello</div>
+            <Home/>
         </Page>
     );
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,14 +1,49 @@
 import React from "react";
 import { alpha, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import { Paper, Typography } from "@mui/material";
+import { Container, Grid, Paper, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+    section: {
+        height: "50vh",
+        backgroundImage:
+            "url(https://res.cloudinary.com/baked-bean/image/upload/v1644159260/hero-home_beyt3b.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "top-center",
+    },
+    container: {
+        height: "100%",
+    },
+    content: {
+        height: "100%",
+    },
+}));
 
 const Home = () => {
+    const styles = useStyles();
     const theme = useTheme();
     return (
         <Box>
-            <Paper elevation={3}>
-                <Typography>Home</Typography>
+            <Paper className={styles.section} elevation={3}>
+                <Container className={styles.container} maxWidth="lg">
+                    <Grid
+                        container
+                        alignItems="center"
+                        className={styles.content}
+                    >
+                        <Grid item xs={12}>
+                            <Typography
+                                fontWeight={600}
+                                color="white"
+                                variant="h2"
+                                component="h1"
+                            >
+                                We love those little a-holes
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Container>
             </Paper>
         </Box>
     );
